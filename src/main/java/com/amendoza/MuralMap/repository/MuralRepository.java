acdf2b5e2e06fbd36964c4f61c968e719f283f5e
Amendoza20 <20.angel.mendoza@gmail.com>
2019-12-26 16:49:01 -0500

    added service methods

--- src/main/java/com/amendoza/MuralMap/repository/MuralRepository.java
+++ src/main/java/com/amendoza/MuralMap/repository/MuralRepository.java
@@ -4,4 +4,9 @@ import com.amendoza.MuralMap.model.Mural;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MuralRepository extends CrudRepository <Mural, Integer> {
+    Mural findMuralByName (String name);
+    Mural findMuralByArtistName (String artistName);
+    Mural findMuralByLocation (String location);
+    Mural findMuralByGenre (String genre);
+
 }
--- src/main/java/com/amendoza/MuralMap/service/MuralService.java
+++ src/main/java/com/amendoza/MuralMap/service/MuralService.java
@@ -1,4 +1,59 @@
 package com.amendoza.MuralMap.service;
 
+import com.amendoza.MuralMap.model.Mural;
+import com.amendoza.MuralMap.repository.MuralRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MuralService {
+    @Autowired
+    private MuralRepository repository;
+
+    public Mural addMural (Mural mural) throws Exception{
+        if(repository.findMuralByName(mural.getName()) == null){
+            return repository.save(mural);
+        }
+        throw new Exception("Mural already exists. Try 'updating' instead?");
+    }
+
+    public Mural updateMural (String name, Mural mural){
+        Mural originalMural = findMuralByName(name);
+        if(mural.getName() != null)
+            originalMural.setName(mural.getName());
+        if(mural.getArtistName() != null)
+            originalMural.setArtistName(mural.getArtistName());
+        if(mural.getMuralDescription() != null)
+            originalMural.setMuralDescription(mural.getMuralDescription());
+        if(mural.getLocation() != null)
+            originalMural.setLocation(mural.getLocation());
+        if(mural.getGenre() != null)
+            originalMural.setLocation(mural.getLocation());
+        return repository.save(mural);
+    }
+
+    public Mural findMuralByName (String name) {
+        return repository.findMuralByName(name);
+    }
+
+    public Mural findMuralByArtistName (String artistName){
+        return repository.findMuralByArtistName(artistName);
+    }
+
+    public Mural findMuralByLocation (String location){
+        return repository.findMuralByLocation(location);
+    }
+
+    public Mural findMuralByGenre (String genre){
+        return repository.findMuralByGenre(genre);
+    }
+
+    public Boolean deleteMural (String name){
+        Mural mural = findMuralByName(name);
+        if(mural != null){
+            repository.delete(mural);
+            return true;
+        }
+        return false;
+    }
 }
