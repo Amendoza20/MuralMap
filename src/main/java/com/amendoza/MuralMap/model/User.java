9a70c306e9b2c72bbb0b5e06594c88cc993feb1b
Amendoza20 <20.angel.mendoza@gmail.com>
2020-01-19 09:34:38 -0500

    Setting up for User function

---
+++ src/main/java/com/amendoza/MuralMap/model/User.java
@@ -0,0 +1,9 @@
+package com.amendoza.MuralMap.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "MuralUsers")
+public class User {
+}
---
+++ src/main/java/com/amendoza/MuralMap/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.amendoza.MuralMap.repository;
+
+import com.amendoza.MuralMap.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository <User, Long> {
+}
